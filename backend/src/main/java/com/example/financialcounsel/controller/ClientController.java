690760e973d2eabcbf7a18a4924f7ac8ae4ea4e4
leedohyung28 <dhleee0123@gmail.com>
2025-12-30 00:53:14 +0900

    [ADD] 직원관련 서비스 컨트롤러 세팅

--- backend/src/main/java/com/example/financialcounsel/controller/ClientController.java
+++ backend/src/main/java/com/example/financialcounsel/controller/ClientController.java
@@ -1,6 +1,25 @@
+package com.example.financialcounsel.controller;
+
+import com.example.financialcounsel.domain.ClientVO;
+import com.example.financialcounsel.service.ClientService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/client")
 public class ClientController {
+    private final ClientService clientService;
 
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
 
+    @PostMapping
+    public List<ClientVO> selectListClient(ClientVO formObject) {
+        List<ClientVO> listClient = clientService.selectListClient(formObject);
+        return listClient;
+    }
 }
--- backend/src/main/java/com/example/financialcounsel/service/ClientService.java
+++ backend/src/main/java/com/example/financialcounsel/service/ClientService.java
@@ -1,3 +1,14 @@
+package com.example.financialcounsel.service;
+
+import com.example.financialcounsel.domain.ClientVO;
+import com.example.financialcounsel.repository.ClientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -6,5 +17,7 @@ public class ClientService {
 
     public List<ClientVO> selectListClient(ClientVO clientVO) {
         List<ClientVO> clientList = new ArrayList<>();
+
+        return clientList;
     }
 }
